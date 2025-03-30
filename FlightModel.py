27e957a908eabd7aca04596c663742285ff04fa4
Ngan-Tang <85889192+Ngan-Tang@users.noreply.github.com>
2025-03-30 00:35:26 -0400

    Update FlightModel.py
    Added epochs to fit() so training is run on the data set multiple times

--- FlightModel.py
+++ FlightModel.py
@@ -7,46 +7,27 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 #Read data
 data = pd.read_csv('iad_lax_flights.csv')
-print(data.head())
+#print(data.head())
 
 data['Departure Date'] = pd.to_datetime(data['Departure Date'])
 data['Departure Timestamp'] = data['Departure Date'].dt.dayofyear
 
-features = ['Flight ID','Departure Airport','Arrival Airport','Departure Timestamp']
-target = 'Price'
-
-label_encoder = LabelEncoder()
-data['Flight ID'] = label_encoder.fit_transform(data['Flight ID'])
-data['Departure Airport'] = label_encoder.fit_transform(data['Departure Airport'])
-data['Arrival Airport'] = label_encoder.fit_transform(data['Arrival Airport'])
-data['Departure Timestamp'] = label_encoder.fit_transform(data['Departure Timestamp'])
-
-
-
 #Preprocessing 
-x = data[features]
-y = data[target]
+x = data[['Departure Timestamp']].values
+y = data['Price'].values
 
 #Scale the features
 scaler = StandardScaler()
-#x_scaled = scaler.fit_transform(x)
+x_scaled = scaler.fit_transform(x)
 
 #Reshape the data for LSTM
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
-
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
-
-x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
-x_test = x_test.reshape((x_test.shape[0], 1, x_test.shape[1]))
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
 
 #Building Model - Sequential API used for simpiler linear model
 model = models.Sequential(
     [
     #3-layer 
-    layers.Input(shape=(x_train.shape[1], x_train.shape[2]), name = "input"), 
-    layers.Dense(64, activation = "relu", name = "layer1"),
+    layers.Dense(64, input_dim=1, activation = "relu", name = "layer1"), #input layer
     layers.Dense(32, activation = "relu", name = "layer2"), 
     layers.Dense(1, name = "predictions")
     ]
@@ -57,22 +38,20 @@ model.compile(
     #Optimizer - Adam to use Stochastic Gradient Descent (SGD) for large datasets
     optimizer="adam",
     # Loss function to minimize
-    loss="mse",
-    # List of metrics to monitor
-    metrics=["mae"]
+    loss="mse"
 )
 
 #Train Model: calling fit()
-model.fit(x_train, y_train, validation_data = (x_test, y_test))
+history = model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs=500, batch_size=32)
 
 #Evaluate Model
-model.evaluate(x_test, y_test)
+loss = model.evaluate(x_test, y_test)
+#print(f"Test Loss: {loss}")
 
 #Generate Prediction
 predictions = model.predict(x_test) 
-comparison = pd.DataFrame({'Actual Price': y_test, 'Predicted Price': predictions.flatten()})
-print(comparison)
-
+#comparison = pd.DataFrame({'Actual Price': y_test, 'Predicted Price': predictions.flatten()})
+#print(comparison)
 
 #Save Model
 model.save('flight_price_model.h5')
